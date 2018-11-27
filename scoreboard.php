0a63915decda010cb640b8fb7364d18d77b52210
Anton Hvornum <anton.feeds@gmail.com>
2018-11-27 12:24:28 +0100

    Revamped the UI to be more simplsitic

---
+++ scoreboard.php
@@ -0,0 +1,217 @@
+<?php
+	// The pure scoreboard is at the bottom
+	// This big chunk of HTML is used for the "TV-mode"
+	if(isset($_GET['mode']) && $_GET['mode'] == 'tv') {
+?>
+<html>
+	<head>
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:site" content="@hexlify" />
+		<meta name="twitter:creator" content="@hexlify" />
+		<meta name="twitter:creator" content="tamm" />
+		<meta property="og:url" content="https://fnite.se/" />
+		<meta property="og:title" content="DHS18 Fortnite BYOC" />
+		<meta property="og:description" content="Dreamhack Summer 2018 - Fortnite BYOC Score submission" />
+		<meta property="og:image" content="https://fnite.se/logo.png" />
+		<!-- A joint effort by Tamm and DoXiD to create a quick tournament site during DreamHack Summer 2018.
+			Needless to say, we put this site together in less than 6 hours, some features were intended, some wasn't.
+			All in the good spirit of hackathon.
+
+			Thread lightly, Ye who enter here.. Here be dragons -->
+		<style type="text/css">
+			@font-face {
+				font-family: LuckyGuy;
+				src: url('/LuckiestGuy-Regular.ttf');
+			}
+
+			:root {
+				--blue: #62CFEE;
+				--pink: #F92472;
+				--green: #A6E22C;
+				--yellow: #E7DB74;
+				--orange: #f60;
+				--moreorange: #66D9EF;
+				--teal: #66D9EF;
+				--darkish: #74705D;
+				--dark: #2a2a2a;
+			}
+
+			body {
+				background-image: url('./background.jpg');
+				background-size: cover;
+				background-position: center center;
+				background-attachment: fixed;
+				margin: 0px;
+				padding: 0px;
+				color: #FFFFFF;
+				font-family: Helvetica Neue,Helvetica,Arial,sans-serif;
+			}
+
+			#content {
+				position: absolute;
+				left: 0px;
+				top: 0px;
+				width: 100%;
+				height: 100%;
+				background-color: #272822;
+
+				column-count: 2;
+				column-width: 50%;
+				column-gap: 2px;
+			}
+
+				#content > div > table {
+					text-align: center;
+					break-inside: avoid;
+					width: 100%;
+				}
+
+					.mode {
+						font-size: 34px;
+						padding: 20px;
+					}
+
+					.pname, .pscore {
+						font-size: 34px;
+						padding: 20px;
+						opacity: 0.8;
+					}
+
+					.name {
+						font-family: Arial, sans-serif;
+						font-weight: bold;
+					}
+
+					.solo {
+						padding-bottom: 0px;
+						padding-top: 40px;
+						font-family: 'LuckyGuy', cursive;
+						color: var(--green);
+					}
+
+					.duo {
+						padding-bottom: 0px;
+						padding-top: 40px;
+						font-family: 'LuckyGuy', cursive;
+						color: var(--blue);
+					}
+		</style>
+	</head>
+<body>
+	<div id="content">
+<?php
+	}
+	//** This is where the TV-Mode ends.
+?>
+<div id="moo">
+	<?php
+		$db_user = 'esport';
+		$db_pass = null;
+		$db_db = 'esport';
+
+		$dbconn = pg_connect("dbname=$db_db user=$db_user password=$db_pass");
+		if (!$dbconn) {
+			die("Could not connect to database");
+		}
+
+		// Create tables if they haven't already been created.
+		// A wise person once said, let there be a class for reused objects...
+		pg_query($dbconn, "CREATE TABLE IF NOT EXISTS fortnite_solo (id SERIAL, player_id VARCHAR(255), placement INT, kills INT, score INT);");
+		pg_query($dbconn, "CREATE TABLE IF NOT EXISTS fortnite_duo (id SERIAL, player_one VARCHAR(255), player_two VARCHAR(255), placement INT, kills INT, score INT);");
+		pg_query($dbconn, "CREATE TABLE IF NOT EXISTS bans (player_id VARCHAR(255));");
+
+		$solo_results = pg_query($dbconn, "SELECT player_id, placement, kills, score FROM fortnite_solo WHERE player_id NOT IN (SELECT player_id FROM bans) ORDER BY score DESC;");
+		$team_results = pg_query($dbconn, "SELECT player_one, player_two, placement, kills, score FROM fortnite_duo WHERE player_one NOT IN (SELECT player_id FROM bans) AND player_two NOT IN (SELECT player_id FROM bans) ORDER BY score DESC;");
+	?>
+	<table>
+		<tr>
+			<th class="mode solo" colspan="2">
+				solo
+			</th>
+		</tr>
+		<tr>
+			<th class="pname">
+				Player Name
+			</th>
+			<th class="pscore">
+				Player score
+			</th>
+		</tr>
+		<?php
+
+			if (!$solo_results || pg_num_rows($solo_results) <= 0) {
+				print '<tr><td colspan="2">There are no scores here yet.</td></tr>';
+			} else {
+				$players = array();
+				while ($row = pg_fetch_assoc($solo_results)) {
+					if(!isset($players[$row['player_id']]))
+						$players[$row['player_id']] = array('player_id' => $row['player_id'], 'games' => array(), 'top_5' => array(), 'total_score_top_5' => 0);
+
+					$game_struct = array('placement' => $row['placement'], 'kills' => $row['kills'], 'score' => $row['score']);
+					if(sizeof($players[$row['player_id']]['top_5']) < 5) {
+						$players[$row['player_id']]['top_5'][] = $game_struct;
+						$players[$row['player_id']]['total_score_top_5'] += $game_struct['score'];
+					}
+				}
+
+				usort($players, function($a, $b){
+					return $a['total_score_top_5'] == $b['total_score_top_5'] ? 0 : $a['total_score_top_5'] < $b['total_score_top_5'] ? 1 : -1;
+				});
+
+				foreach($players as $player_id => $data) {
+					print "<tr><td class='name'>" . $data['player_id'] . "</td><td>" . $data['total_score_top_5'] . "</td></tr>";
+				}
+			}
+
+		?>
+	</table>
+</div>
+<div id="moo2">
+	<table>
+		<tr>
+			<th class="mode duo" colspan="2">
+				duo
+			</th>
+		</tr>
+		<tr>
+			<th class="pname">
+				Player Names
+			</th>
+			<th class="pscore">
+				Player score
+			</th>
+		</tr>
+		<?php
+			if (!$team_results || pg_num_rows($team_results) <= 0) {
+				print '<tr><td colspan="2">There are no scores here yet.</td></tr>';
+			} else {
+				$teams = array();
+				while ($row = pg_fetch_assoc($team_results)) {
+					if ($row['player_one'].$row['player_two'] == '') {
+						continue;
+					}
+					$team = $row['player_one'].', '.$row['player_two'];
+
+					if(!isset($teams[$team]))
+						$teams[$team] = array('player_id' => $team, 'games' => array(), 'top_5' => array(), 'total_score_top_5' => 0);
+
+					$game_struct = array('placement' => $row['placement'], 'kills' => $row['kills'], 'score' => $row['score']);
+					$teams[$team]['games'][] = $game_struct;
+					if(sizeof($teams[$team]['top_5']) < 5) {
+						$teams[$team]['top_5'][] = $game_struct;
+						$teams[$team]['total_score_top_5'] += $game_struct['score'];
+					}
+				}
+
+				usort($teams, function($a, $b){
+					return $a['total_score_top_5'] == $b['total_score_top_5'] ? 0 : $a['total_score_top_5'] < $b['total_score_top_5'] ? 1 : -1;
+				});
+
+				foreach($teams as $player_id => $data) {
+					print "<tr><td class='name'>" . $data['player_id'] . "</td><td>" . $data['total_score_top_5'] . "</td></tr>";
+				}
+			}
+
+		?>
+	</table>
+</div>
--- upload.php
+++ upload.php
@@ -6,61 +6,113 @@
 	//}
 
 	header('Content-type: text/javascript');
-	
-	$data = json_decode(file_get_contents('php://input'), true);
-	$db_user = null;
+
+	$__LOCKED = array('solo' => false, 'duo' => false);
+	$db_user = "esport";
 	$db_pass = null;
-	$db_db = null;
+	$db_db = "esport";
+	$target_dir = "./uploads/";
+
 	$placement_map = array(1 => 80, 2 => 60, 3 => 40, 4 => 20, 5 => 10);
 	$kill_multiplier = 5;
 
-	$player_id = pg_escape_string($data['player_id']);
-	$placement = pg_escape_string($data['placement']);
-	$kills = pg_escape_string($data['kills']);
-	$screenshot = pg_escape_string($data['screenshot']);
+	$dbconn = pg_connect("dbname=$db_db user=$db_user password=$db_pass");
+
+	// Generally these won't be needed, because the landing page (scoreboard) sets em up.
+	// But hey, livrem å hängslen!
+	pg_query($dbconn, "CREATE TABLE IF NOT EXISTS fortnite_solo (id SERIAL, player_id VARCHAR(255) NOT NULL, placement INT NOT NULL, kills INT NOT NULL, score INT NOT NULL, screenshot VARCHAR(64) NOT NULL, UNIQUE(player_id, screenshot));");
+	pg_query($dbconn, "CREATE TABLE IF NOT EXISTS fortnite_duo (id SERIAL, player_one VARCHAR(255) NOT NULL, player_two VARCHAR(255) NOT NULL, placement INT NOT NULL, kills INT NOT NULL, score INT NOT NULL, screenshot VARCHAR(64) NOT NULL, UNIQUE(player_one, player_two, screenshot));");
+	pg_query($dbconn, "CREATE TABLE IF NOT EXISTS bans (player_id VARCHAR(255));");
+
+	function gen_uuid() {
+		return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+			mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0xfff ) | 0x4000,
+			mt_rand( 0, 0x3ff ) | 0x8000,
+			mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+		);
+	}
+
+	// Make sure the upload directory exists.
+	// If this fails, well a clever admin surely gotta check the error log and fix it.. right?
+	if (!file_exists($target_dir)) {
+		mkdir($target_dir, 0777, true);
+	}
+
+	if(isset($_FILES['screenshot']) && filesize($_FILES["screenshot"]["tmp_name"]) > 0) {
+		$hash = hash_file("sha256", $_FILES["screenshot"]["tmp_name"]);
+		$file_id = gen_uuid();
+		$extension = pathinfo($_FILES["screenshot"]["name"], PATHINFO_EXTENSION);
+		$final_destination = $target_dir . $file_id .'.'. $extension;
+	} else {
+		die(json_encode(array('result' => 'failed', 'reason' => 'No screenshot was attached.')));
+	}
+
+	while (file_exists($final_destination)) {
+		$file_id = gen_uuid();
+		$final_destination = $target_dir . $file_id .'.'. $extension;
+	}
+
+	$placement = pg_escape_string($_POST['placement']);
+	$kills = pg_escape_string($_POST['kills']);
+
 	$score = 0;
 	if(isset($placement_map[$placement]))
 		$score += $placement_map[$placement];
 	$score += $kill_multiplier * $kills;
 
-	$dbconn = pg_connect("dbname=$db_db user=$db_user password=$db_pass");
-
-	$tmp = explode(',', $player_id);
-	$players = array();
-	foreach($tmp as $player) {
-		$players[] = trim($player);
-	}
-
-	if(sizeof($players) == 1) {
-		print json_encode(array('result' => 'failed', 'reason' => 'Solo results are locked in! No more scores are allowed.'));
-		die();
-		// Solo
-		$result = pg_query($dbconn, "SELECT id FROM fortnite_solo WHERE screenshot='".$screenshot."';");
+	// Solo
+	if(isset($_POST['player_id']) && !isset($_POST['player_one']) && !isset($_POST['player_two'])) {
+		if($__LOCKED['solo']) {
+			die(json_encode(array('result' => 'failed', 'reason' => 'Solo results are locked in! No more scores are allowed.')));
+		}
+		
+		$result = pg_prepare($dbconn, "find_screenshot", 'SELECT id FROM fortnite_solo WHERE screenshot=$1;');
+		$result = pg_execute($dbconn, "find_screenshot", array($hash));
+	
 		if($result) {
-			while ($row = pg_fetch_assoc($result)) {
-				print json_encode(array('result' => 'failed', 'reason' => 'Screenshot already uploaded once in solo.'));
-				die();
+			if(pg_num_rows($result)) {
+				while ($row = pg_fetch_assoc($result)) {
+					die(json_encode(array('result' => 'failed', 'title' => 'Registration failed', 'message' => 'Screenshot already uploaded once in solo game.')));
+				}
 			}
+		} else {
+			die(json_encode(array('result' => 'failed', 'title' => 'Database error', 'message' => pg_last_error($dbconn))));
 		}
 
-		$result = pg_prepare($dbconn, "my_query", 'INSERT INTO fortnite_solo (player_id, placement, kills, score, screenshot) VALUES ($1, $2, $3, $4, $5);');
-		$result = pg_execute($dbconn, "my_query", array($players[0], $placement, $kills, $score, $screenshot));
+		$result = pg_prepare($dbconn, "update_solo", 'INSERT INTO fortnite_solo (player_id, placement, kills, score, screenshot) VALUES ($1, $2, $3, $4, $5);');
+		$result = pg_execute($dbconn, "update_solo", array($_POST['player_id'], $placement, $kills, $score, $hash));
+	
+		error_log("Player $player_id got $kills kills at placement $placement.");
+
+	// Duo
 	} else {
-		// Duo
-		print json_encode(array('result' => 'failed', 'reason' => 'Duo results are locked in! No more scores are allowed.'));
-		die();
-		$result = pg_query($dbconn, "SELECT id FROM fortnite_duo WHERE screenshot='".$screenshot."';");
-		if($result) {
-			while ($row = pg_fetch_assoc($result)) {
-				print json_encode(array('result' => 'failed', 'reason' => 'Screenshot already uploaded once in duo.'));
-				die();
-			}
+		if($__LOCKED['duo']) {
+			die(json_encode(array('result' => 'failed', 'reason' => 'Duo results are locked in! No more scores are allowed.')));
 		}
+		
+		$result = pg_prepare($dbconn, "find_screenshot", 'SELECT id FROM fortnite_duo WHERE screenshot=$1;');
+		$result = pg_execute($dbconn, "find_screenshot", array($hash));
+
+		if($result) {
+			if(pg_num_rows($result)) {
+				while ($row = pg_fetch_assoc($result)) {
+					die(json_encode(array('result' => 'failed', 'reason' => 'Screenshot already uploaded once in duo.')));
+				}
+			}
+		} else {
+			die(json_encode(array('result' => 'failed', 'title' => 'Database error', 'message' => pg_result_error($result))));
+		}
+
+		$players = array($_POST['player_one'], $_POST['player_two']);
 		sort($players);
-		$result = pg_prepare($dbconn, "my_query", 'INSERT INTO fortnite_duo (player_one, player_two, placement, kills, score, screenshot) VALUES ($1, $2, $3, $4, $5, $6);');
-		$result = pg_execute($dbconn, "my_query", array($players[0], $players[1], $placement, $kills, $score, $screenshot));
+
+		$result = pg_prepare($dbconn, "update_duo", 'INSERT INTO fortnite_duo (player_one, player_two, placement, kills, score, screenshot) VALUES ($1, $2, $3, $4, $5, $6);');
+		$result = pg_execute($dbconn, "update_duo", array($players[0], $players[1], $placement, $kills, $score, $hash));
+
+		error_log("Playes " .$players[0]. " & " .$players[1]. " got $kills kills at placement $placement.");
 	}
 
-	error_log("Player $player_id got $kills kills at placement $placement.");
-	print json_encode(array('result' => 'success'));
+	print json_encode(array('result' => 'success', "title" => "Registration successful", "message" => "Your score has been counted"));
 ?>
